307969f6f0ffcce9234ecb55ee7583318efcb40b
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-05-14 10:10:40 +0200

    generate: formation spring webmvc

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
 
     // Titre
-    title: "Formation Spring Data",
+    title: "Formation Spring Web MVC",
 
     // Nom de l'organisation
     repoUser: "DevInstitut",
 
     // Nom du dépôt dans l'organisation Github
-    repoName: "formation-spring-data",
+    repoName: "formation-spring-webmvc",
 
     context: `
 Ce cours est donné dans le cadre d'un cursus de 60 jours visant à donner aux stagiaires la double compétence Spring / Angular.
 
 > Il s'agit ici d'un support de présentation, le contenu a été écrit dans l'objectif d'être animé par un formateur.
 
-Ce contenu est habituellement déroulé en 1 jour de formation.
+Ce contenu est habituellement déroulé en 2 jours de formation.
     `
 };
 
