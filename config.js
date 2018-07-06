ba98bd7b1652ad6abba3650c1d6d1fd1bdd3e3d9
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-07-06 16:49:38 +0200

    Spring 07-2018

--- config.js
+++ config.js
@@ -1,21 +1,29 @@
 const CONFIG = {
 
     // Titre
-    title: "Formation Spring Context",
+    title: "Formation Spring",
 
     // Nom de l'organisation
     repoUser: "DevInstitut",
 
     // Nom du dépôt dans l'organisation Github
-    repoName: "formation-spring-context",
+    repoName: "formation-spring-07-2018",
 
     context: `
 Ce cours est donné dans le cadre d'un cursus de 60 jours visant à donner aux stagiaires la double compétence Spring / Angular.
 
 > Il s'agit ici d'un support de présentation, le contenu a été écrit dans l'objectif d'être animé par un formateur.
+Il a été créé sur-mesure après qualification des stagiaires. 
 
-Ce contenu est habituellement déroulé en 1 jour de formation.
-    `
+Ce contenu est habituellement déroulé en 7 jours de formation.
+
+Les sujets abordés :
+* Spring Boot
+* Spring Context (Injection de dépendances)
+* Spring accès aux données (spring-jdbc, spring-orm, spring-data-jpa)
+* Spring Web MVC
+* Spring Security
+`
 };
 
 module.exports = CONFIG;
