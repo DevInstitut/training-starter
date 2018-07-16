808e63b7ae07f19364ff1c0f16f948689df3a1d0
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-07-16 10:36:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
 const CONFIG = {
 
     // Titre
-    title: "Workshop - Play! - Activity App",
+    title: "Formation Angular",
 
     // Nom de l'organisation
     repoUser: "DevInstitut",
 
     // Nom du dépôt dans l'organisation Github
-    repoName: "workshop-play-activity-app",
+    repoName: "angular-07-2018",
 
     context: `
+Ce cours est donné dans le cadre d'un cursus de 60 jours visant à donner aux stagiaires la double compétence Spring / Angular.
+
+C'est pour cela, que certains travaux pratiques font référence à Spring.
+
 > Il s'agit ici d'un support de présentation, le contenu a été écrit dans l'objectif d'être animé par un formateur.
 
-Ce contenu est habituellement déroulé en 3 jours de formation.
-    `
+Ce contenu est habituellement déroulé en 6 jours de formation.`
 };
 
 module.exports = CONFIG;
